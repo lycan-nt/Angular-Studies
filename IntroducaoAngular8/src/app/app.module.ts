95d8177217c62c9e67df34ba89e8f3d76c2672f9
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-15 05:47:42 -0300

    Ativando rotas

--- IntroducaoAngular8/src/app/app.module.ts
+++ IntroducaoAngular8/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { Error404Component } from './error-404/error-404.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +29,10 @@ import { Error404Component } from './error-404/error-404.component';
         path: '', 
         redirectTo: 'courses',
         pathMatch: 'full'
+      },
+      {
+        path: "courses/info/:id",
+        component: CourseInfoComponent
       },
       {
         path: 'courses',
