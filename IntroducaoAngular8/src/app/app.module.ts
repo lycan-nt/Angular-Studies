5bc4d7a7279267a2b551388bf8bbde4bf5d6c550
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-24 14:48:45 -0300

    Core

--- IntroducaoAngular8/src/app/app.module.ts
+++ IntroducaoAngular8/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/component/nav-bar/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component,
+    Error404Component
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', 
