584eacbd2a54e6ab73c540af55c9c053d47dd683
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-24 15:09:13 -0300

    Errors

--- IntroducaoAngular8/src/app/app.module.ts
+++ IntroducaoAngular8/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
-import { CoreModule } from './core/component/nav-bar/core.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     HttpClientModule,
@@ -23,10 +21,6 @@ import { CoreModule } from './core/component/nav-bar/core.module';
         redirectTo: 'courses',
         pathMatch: 'full'
       },
-      {
-        path: "**",
-        component: Error404Component
-      }
     ])
   ],
   providers: [],
