b6f85c5c40bb20ba875c4c84154e1ab01942c6bf
Felipe D. Santos <lycan.felipe@gmail.com>
2021-10-27 07:10:12 -0300

    Angular Flex-Layout

--- Angular8-FlexLayout/GenericWebStartup/src/app/app.component.ts
+++ Angular8-FlexLayout/GenericWebStartup/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit, OnDestroy{
+
+  myControl = new FormControl();
+  options: string[] = ['Chennai', 'Salem', 'Coibatore'];
 
   private mediaSub: Subscription;
   constructor(
