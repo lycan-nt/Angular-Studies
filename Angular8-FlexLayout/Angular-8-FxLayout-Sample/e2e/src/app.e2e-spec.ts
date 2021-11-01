308b0d7b50c8fcfaf726e19b82d1651bd451191a
Felipe D. Santos <lycan.felipe@gmail.com>
2021-11-01 06:52:16 -0300

    Angular Flex-Layout

--- Angular8-FlexLayout/Angular-8-FxLayout-Sample/e2e/src/app.e2e-spec.ts
+++ Angular8-FlexLayout/Angular-8-FxLayout-Sample/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to GenericWebStartup!');
+    expect(page.getTitleText()).toEqual('GenericWebStartup app is running!');
   });
 
   afterEach(async () => {
--- Angular8-FlexLayout/Angular-8-FxLayout-Sample/src/app/app.module.ts
+++ Angular8-FlexLayout/Angular-8-FxLayout-Sample/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from  '@angular/forms'
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './core/material.module';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +22,7 @@ import { MaterialModule } from './core/material.module';
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
