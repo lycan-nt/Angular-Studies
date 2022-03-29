fc1150153bcb587b3567a35dc544b196014505ed
Felipe D. Santos <lycan.felipe@gmail.com>
2022-03-28 21:22:05 -0300

    Directives

--- Angular9/crud/frontend/src/app/app.module.ts
+++ Angular9/crud/frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomeComponent } from './views/home/home.component';
 import { MatCardModule } from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
