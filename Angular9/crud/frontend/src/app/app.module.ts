c696a633c7ccb0f0a16f005ee2686d81f6c053a6
Felipe D. Santos <lycan.felipe@gmail.com>
2022-04-11 19:27:03 -0300

    Create product

--- Angular9/crud/frontend/src/app/app.module.ts
+++ Angular9/crud/frontend/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -39,7 +41,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
     
   ],
   providers: [],
--- Angular9/crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ Angular9/crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Router } from '@angular/router';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,6 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0.00
+  }
+
   constructor(
     private productService: ProductService,
     private router: Router
@@ -19,7 +25,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage('Bora dominar o mundo!');
+      this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Bora dominar o mundo!');
+      this.router.navigate(['/products']);
+    })
   }
 
   cancell(): void {
