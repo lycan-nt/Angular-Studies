64376b320c20e3bbf221bb223a0daeb7739bccbe
Felipe D. Santos <lycan.felipe@gmail.com>
2022-04-25 20:33:22 -0300

    Edit Product

--- Angular9/crud/frontend/src/app/components/product/product.service.ts
+++ Angular9/crud/frontend/src/app/components/product/product.service.ts
@@ -33,12 +33,12 @@ export class ProductService {
   }
 
   readById(id: string): Observable<Product> {
-    const url = `${this.baseUrl}/${id}}`;
+    const url = `${this.baseUrl}/${id}`;
     return this.http.get<Product>(url);
   }
 
   update(product: Product): Observable<Product> {
-    const url = `${this.baseUrl}/${product.id}}`;
+    const url = `${this.baseUrl}/${product.id}`;
     return this.http.put<Product>(url, product);
   }
 }
