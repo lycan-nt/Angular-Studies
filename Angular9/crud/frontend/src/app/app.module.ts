94561baa0a6a85b66f6d26db3b5d6e5967927342
Felipe D. Santos <lycan.felipe@gmail.com>
2022-04-26 20:52:16 -0300

    Component DeleteProduct

--- Angular9/crud/frontend/src/app/app.module.ts
+++ Angular9/crud/frontend/src/app/app.module.ts
@@ -25,7 +25,8 @@ import { ProductReadComponent } from './components/product/product-read/product-
 import {MatTableModule} from '@angular/material/table';
 import localePt from '@angular/common/locales/pt'
 import { registerLocaleData } from '@angular/common';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'
 
 registerLocaleData(localePt);
 
@@ -41,7 +42,8 @@ registerLocaleData(localePt);
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductUpdateComponent
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
