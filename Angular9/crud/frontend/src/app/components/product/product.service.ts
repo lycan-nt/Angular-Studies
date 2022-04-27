d2fd924bba03db9731ef6ef8a0ede04f57e8f1a3
Felipe D. Santos <lycan.felipe@gmail.com>
2022-04-27 20:49:13 -0300

    Update titles with   private _headerData = new BehaviorSubject<HeaderData>({

--- Angular9/crud/frontend/src/app/components/product/product.service.ts
+++ Angular9/crud/frontend/src/app/components/product/product.service.ts
@@ -39,8 +39,6 @@ export class ProductService {
     );
   }
 
-  
-
   readById(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
     return this.http.get<Product>(url).pipe(
