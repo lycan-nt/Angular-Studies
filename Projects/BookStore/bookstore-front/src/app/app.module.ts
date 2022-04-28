9d1cba1e1ad749f7362465db4efd63105262cfe7
Felipe D. Santos <lycan.felipe@gmail.com>
2022-04-28 06:18:03 -0300

    Component CreateCategoria inplement css

--- Projects/BookStore/bookstore-front/src/app/app.module.ts
+++ Projects/BookStore/bookstore-front/src/app/app.module.ts
@@ -17,6 +17,9 @@ import {MatTableModule} from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
 import {MatButtonModule} from '@angular/material/button';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -39,7 +42,10 @@ import { CategoriaCreateComponent } from './components/views/categoria/categoria
     MatCardModule,
     MatTableModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
