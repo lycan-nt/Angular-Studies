68cf9af4e353a83a070167ed7caa8a6b5678864a
Felipe D. Santos <lycan.felipe@gmail.com>
2022-05-20 06:11:27 -0300

    Update categories

--- Projects/BookStore/bookstore-front/src/app/components/views/categoria/categoria-update/categoria-update.component.ts
+++ Projects/BookStore/bookstore-front/src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriaService } from './../categoria.service';
+import { Categoria } from './../categoria.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaUpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
   }
 
+  constructor(
+    private service: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe((resp) => {
+      this.categoria = resp;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.categoria).subscribe((resp) => {
+    this.router.navigate(["categorias"]);
+    this.service.mensagem("Categória atualizada com sucesso!");
+    },
+    err => {
+      console.log(err);
+    }
+    )
+  }
+
+  cancel(): void {
+    this.router.navigate(['categorias']);
+  }
 }
