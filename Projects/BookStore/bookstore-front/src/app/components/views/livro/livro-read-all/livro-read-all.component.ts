882300ed0ea78a88f59eda6f822b0dc087649b79
Felipe D. Santos <lycan.felipe@gmail.com>
2022-05-24 05:56:16 -0300

    Read Books For Categories

--- Projects/BookStore/bookstore-front/src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ Projects/BookStore/bookstore-front/src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
+import { Livro } from './../livro.model';
+import { LivroService } from './../livro.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class LivroReadAllComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'titulo','livros', 'acoes'];
+  id_cat: String = '';
+  livros: Livro[] = [];
 
-  constructor() { }
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAllByCategoria(this.id_cat).subscribe((resp) => {
+      this.livros = resp;
+      console.table(this.livros);
+    })
   }
 
 }
